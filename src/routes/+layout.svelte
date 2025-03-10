41eff25c4c39f55f2021733a7c9211c484780f2b
jonas <jonas.voisard@gmail.com>
2025-03-10 22:03:14 +0100

    add 'n' shortcut

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
-<script>
+<script lang="ts">
   import '../app.css'
   import { Toaster } from 'svelte-sonner'
   import TopBar from '$lib/layout/TopBar.svelte'
+  import { onMount } from 'svelte'
+  import { goto } from '$app/navigation'
+  import { page } from '$app/stores'
 
   export let data
+
+  onMount(() => {
+    function shortcutHandler(event: KeyboardEvent) {
+      if (!data.user || !event.ctrlKey) return
+      if (event.key == 'n' && $page.route.id !== '/log') return goto('/log')
+    }
+    window.addEventListener('keydown', shortcutHandler)
+    return () => {
+      window.removeEventListener('keydown', shortcutHandler)
+    }
+  })
 </script>
 
 <Toaster />
